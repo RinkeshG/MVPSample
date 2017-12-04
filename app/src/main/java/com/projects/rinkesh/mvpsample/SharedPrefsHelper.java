0f81ac4877995bcf84e979a4ac9e8d5e9b2001b3
RinkeshG <saviorindiaservice@gmail.com>
2017-12-04 12:31:02 +0530

    Initial commit

---
+++ app/src/main/java/com/projects/rinkesh/mvpsample/SharedPrefsHelper.java
@@ -0,0 +1,12 @@
+package com.projects.rinkesh.mvpsample;
+
+/**
+ * Created by Rinkesh on 04/12/17.
+ */
+
+public class SharedPrefsHelper {
+
+    public static final String MY_PREFS = "MY_PREFS";
+
+    
+}
